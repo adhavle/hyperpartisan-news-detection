8fd22c7e1895a5d32a37d65c34833b1e969fd31a
ADhavle <adhavle@users.noreply.github.com>
2025-08-14 16:24:25 -0700

    Initial commit for tool to clean data files

---
+++ xml-data-cleaner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xml_data_cleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
